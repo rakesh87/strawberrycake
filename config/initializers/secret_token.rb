ffaea4d379f8f4770a9a67f870d03ea0b3ac4af3
Flavia Fortes <flaviafortes88@gmail.com>
2013-07-18 13:56:56 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Strawberrycake::Application.config.secret_token = '7330a91afccc01db7830b42b157b6407dddfbe8ac4c642a642150bc378988e4d35194a397b6954c2d25d65c1427018c965b160e432f469e0d8fda42ab03937d5'
