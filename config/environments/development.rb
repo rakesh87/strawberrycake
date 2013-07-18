8f58cc9055a17f2bda9a6022da7a2f5ae8b25e8c
Flavia Fortes <flaviafortes88@gmail.com>
2013-07-18 13:56:59 -0300

    Adding letter_opener config.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Strawberrycake::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Letter openner config
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
