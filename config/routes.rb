565c69c24405f322f77b1dd97ea4e2fc376cb083
Flavia Fortes <flaviafortes88@gmail.com>
2013-07-23 18:41:42 -0300

    Create route to post#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Strawberrycake::Application.routes.draw do
 
   root :to => "posts#index"
+  resources :posts, only: [:new]
+
 end
--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -4,8 +4,14 @@ require "spec_helper"
 describe PostsController do
   describe "routing" do
 
-   it "routes to #index" do
+    it "routes to #index" do
       get(root_path).should route_to("posts#index")
+      root_path.should eq('/')
+    end
+
+    it "routes to #new" do
+      get(new_post_path).should route_to("posts#new")
+      new_post_path.should eq('/posts/new')
     end
   end
 end
