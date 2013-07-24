8d90b3584b8614be56d0354df1f148a457ab2acf
Flavia Fortes <flaviafortes88@gmail.com>
2013-07-24 14:12:10 -0300

    Implement PostController#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,8 @@ class PostsController < ApplicationController
 
   def new
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -22,11 +22,44 @@ describe PostsController do
   end
 
   describe "GET 'new'" do
+
     before do
       get :new
     end
 
     it { should respond_with(:success) }
     it { should render_template(:new) }
+    it { should render_with_layout(:application) }
+  end
+
+  describe "GET 'show'" do
+
+    context "with a valid param" do
+
+      let!(:post) do
+        create(:post)
+      end
+
+      before do
+        get :show, id: post
+      end
+
+      it { should respond_with(:success) }
+      it { should render_template(:show) }
+      it { should render_with_layout(:application) }
+
+      it "should assign post" do
+        assigns(:post).should eq(post)
+      end
+    end
+    
+    context "with a invalid param" do
+
+      it "should raise error" do
+          expect do
+            get :show, id: 'wrong'
+          end.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end  
   end
 end
