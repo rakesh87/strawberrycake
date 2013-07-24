25dd18523e4965db401a49a3387323135e739214
Flavia Fortes <flaviafortes88@gmail.com>
2013-07-24 18:01:24 -0300

    Create PagesController#home

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,4 +2,15 @@ require 'spec_helper'
 
 describe PagesController do
 
+  describe "GET 'index'" do
+
+    before do
+      get :home
+    end
+
+    it { should respond_with(:success) }
+    it { should render_template(:home) }
+    it { should render_with_layout(:application) }
+  end
+
 end
