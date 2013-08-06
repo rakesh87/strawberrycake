d370bee5cfdac17eb39cc34bef797766c2e72542
Flavia Fortes <flaviafortes88@gmail.com>
2013-08-06 15:08:39 -0300

    Add some validation on model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
 
-  validates :name, :email, presence: true
+  validates :uid, :email, :access_token, :provider, presence: true
+  validates :uid, :email, uniqueness: true
+  
   has_many :posts, dependent: :destroy
+
+  def self.find_or_create_with_omniauth(auth)
+    user = self.find_or_create_by_provider_and_uid(auth.provider, auth.uid)
+    user.assign_attributes({ name: auth.info.name, email: auth.info.email, photo_url: auth.info.image, access_token: auth.credentials.token })
+    user.save!
+    user
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,9 @@
 require 'spec_helper'
 
 describe User do
-  describe "mass assignment" do
-    [:name, :email].each do |attr|
-      it { should allow_mass_assignment_of(attr) }
-    end
-  end
 
   describe "validations" do
-    [:name, :email].each do |attr|
+    [:uid, :email, :access_token, :provider].each do |attr|
       it { should validate_presence_of(attr) }
     end
   end
