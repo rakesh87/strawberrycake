e565df13bdd25b58f57350253836e0b0d354a3ff
Flavia Fortes <flaviafortes88@gmail.com>
2013-08-06 17:33:58 -0300

    Add new rout

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Strawberrycake::Application.routes.draw do
   match "/auth/:provider/callback" => "sessions#create", as: :auth_callback
   match "/auth/failure" => "sessions#failure", as: :auth_failure
   match "/logout" => "sessions#destroy", as: :logout
+
+  match "/secret_page" => "secret#index",     as: :secret_page, via: :get
 end
