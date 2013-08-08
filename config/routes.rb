76c4d70ec0c9953ae2c3e2667d7b0333db8b3745
Flavia Fortes <flaviafortes88@gmail.com>
2013-08-08 16:04:52 -0300

    Refactore routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Strawberrycake::Application.routes.draw do
   resources :posts
 
   get "/auth/:provider/callback" => "sessions#create", as: :auth_callback
-  match "/auth/failure" => "sessions#failure", as: :auth_failure
+  get "/auth/failure" => "sessions#failure", as: :auth_failure
   delete "/logout" => "sessions#destroy", as: :logout
 end
