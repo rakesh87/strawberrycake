03f97312d7ecf0e423ed3fc9c2b035c667b31f74
Flavia Fortes <flaviafortes88@gmail.com>
2013-08-12 18:52:41 -0300

    Add authored_by method on model Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ActiveRecord::Base
 
   validates :title, :content, presence: true
   belongs_to :user
+
+  def authored_by?(author)
+    user == author
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -17,4 +17,25 @@ describe Post do
     it { should belong_to(:user) }
   end
 
+  describe "#authored_by?" do
+
+    subject { post.authored_by? user }
+
+    let(:post) { create(:post) }
+
+    context "post author" do
+
+      let(:user) { post.user }
+
+      it { should be_true }
+    end
+
+    context "anyone else" do
+      
+      let(:user) { create(:user) }
+
+      it { should be_false }
+    end
+
+  end
 end
