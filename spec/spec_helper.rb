82485e482cef22947071a9e0c9621ad07abcab24
Flavia Fortes <flaviafortes88@gmail.com>
2013-10-05 23:13:39 -0300

    Add use transactional fixtures true on spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,15 @@
 
 if ENV['coverage'] == 'on'
   require 'simplecov'
-  SimpleCov.start 'rails'
+  SimpleCov.start 'rails' do
+    minimum_coverage 100
+  end
 end
 
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'valid_attribute'
 require "email_spec"
 
 # Requires supporting ruby files with custom matchers and macros, etc,
@@ -17,6 +18,22 @@ require "email_spec"
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  # ## Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
+
   # Use the new rspec expect syntax
   config.expect_with :rspec do |c|
     c.syntax = :expect
